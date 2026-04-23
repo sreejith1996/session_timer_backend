84a154be20d1dbb9081bd07b5259a97879fae944
sreejith1996 <pisharodysreejith96@gmail.com>
2026-04-23 14:11:19 +0000

    load from dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express';
 import type { Application } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Application = express();
 const PORT = Number(process.env.PORT) || 8000;
 
+app.get('/', (req, res) => {
+  res.send('Hello world')
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
