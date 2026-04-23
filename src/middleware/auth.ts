de38d89f0c0c359ec88d26009f13eeb3e649cbb6
sreejith1996 <pisharodysreejith96@gmail.com>
2026-04-23 19:22:25 +0000

    adde supabase authentication to the backend

---
+++ src/middleware/auth.ts
@@ -0,0 +1,30 @@
+import { createClient } from '@supabase/supabase-js';
+import { Request, Response, NextFunction } from 'express';
+
+const supabaseAdmin = createClient(
+    process.env.SUPABASE_URL || '',
+    process.env.SUABASE_SERVICE_ROLE_KEY || ''
+)
+
+async function authenticate(req: Request, res: Response, next: NextFunction) {
+    const authHeader = req.headers.authorization;
+
+    if(!authHeader || !authHeader.startsWith('Bearer ')){
+        return res.status(401).json({ error: 'Missing or invalid Authorization header'});
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    const { data: { user }, error } = await supabaseAdmin.auth.getUser(token);
+
+    if(error || !user) {
+        return res.status(401).json({ error: 'Invalid or expired token' })
+    }
+
+    req.user = user;
+    req.token = token
+    next()
+
+}
+
+export { authenticate };
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,61 @@
 import express from 'express';
 import type { Application } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+import { createClient } from '@supabase/supabase-js';
 
 dotenv.config();
 
 const app: Application = express();
 const PORT = Number(process.env.PORT) || 8000;
 
-app.get('/', (req, res) => {
+app.use(cors({ origin: 'https://laughing-spork-q76gvvj4pgp24xx4-5173.app.github.dev' }));
+
+
+function createUserClient(accessToken : string) {
+  console.log(accessToken);
+  return createClient(
+    process.env.SUPABASE_URL || '',
+    process.env.SUABASE_SERVICE_ROLE_KEY || '',
+    {
+      global: {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        }
+      }
+    }
+  )
+}
+
+app.get('/api/protected', async (req, res) => {
+  try {
+    if (!req.user || !req.token) {
+      console.log(req.user, req.token);
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const userId = req.user.id;
+    const email = req.user.email;
+
+    const supabase = createUserClient(req.token)
+
+    const { data, error } = await supabase
+      .from('sessions')
+      .select('*')
+      .eq('user_id', userId)
+
+    if(error) throw error;
+
+    res.json({
+      message: `Hello ${email}`,
+      userId,
+      info:data
+    })
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : String(error);
+    console.error(error)
+    res.status(500).json({ error: message})
+  }
   res.send('Hello world')
 });
 
